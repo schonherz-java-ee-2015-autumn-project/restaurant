a78156b9d97f2955bdbfc8bfcda25962b96ff2e2
Kovács-Ferenc Norbert <kfnorbi@gmail.com>
2015-12-03 14:51:35 +0100

    Basic project structure Basic maven structure, junit management and compiler configurations

---
+++ resturant-web/src/main/java/org/resturant/web/App.java
@@ -0,0 +1,13 @@
+package org.resturant.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
