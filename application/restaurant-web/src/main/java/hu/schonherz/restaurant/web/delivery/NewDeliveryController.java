5dce1b5858d5bdd5cf306c81a080a897bddc69ba
Péter Dudás <dupetya@gmail.com>
2015-12-20 22:12:56 +0100

    Product dialog started

--- application/restaurant-web/src/main/java/hu/schonherz/restaurant/web/delivery/NewDeliveryController.java
+++ application/restaurant-web/src/main/java/hu/schonherz/restaurant/web/delivery/NewDeliveryController.java
@@ -2,6 +2,7 @@ package hu.schonherz.restaurant.web.delivery;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -24,6 +25,7 @@ public class NewDeliveryController implements Serializable {
 	private OrderVo selectedOrder;
 
 	private boolean canModify;
+	private boolean canModifyProduct;
 
 	@PostConstruct
 	public void init() {
@@ -33,12 +35,30 @@ public class NewDeliveryController implements Serializable {
 		delivery.setOrders(new ArrayList<>());
 
 		canModify = false;
+		canModifyProduct = false;
 	}
 
-	public void onAddOrderButtonClick(ActionEvent e) {
+	public void initOrder() {
 		selectedOrder = new OrderVo();
 		selectedOrder.setOrderState(State.AVAILABLE);
 		selectedOrder.setProducts(new ArrayList<>());
+	}
+
+	public void onAddOrderButtonClick(ActionEvent e) {
+		initOrder();
+	}
+
+	public void onSaveOrderButtonClick(ActionEvent e) {
+		if ("".equals(selectedOrder.getAddress().trim()) || selectedOrder.getDeadline().before(new Date())
+				|| selectedOrder.getProducts().isEmpty()) {
+			// TODO add error message
+			return;
+		}
+		// TODO save order to delivery, update, hide dialog
+	}
+
+	public void onCancelOrderButtonClick(ActionEvent e) {
+		initOrder();
 	}
 
 	public void onOrderRowSelect(SelectEvent e) {
@@ -83,6 +103,14 @@ public class NewDeliveryController implements Serializable {
 
 	public void setCanModify(boolean canModify) {
 		this.canModify = canModify;
+	}
+
+	public boolean isCanModifyProduct() {
+		return canModifyProduct;
+	}
+
+	public void setCanModifyProduct(boolean canModifyProduct) {
+		this.canModifyProduct = canModifyProduct;
 	}
 
 }
